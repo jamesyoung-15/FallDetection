6652a75df0f7caa56fa92b0cb2b09144a0396b04
James <jyyoungaa@connect.ust.hk>
2024-01-08 17:58:01 +0800

    update

--- camera_stream/test-code/camera-http-stream.py
+++ camera_stream/test-code/camera-http-stream.py
@@ -37,4 +37,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
